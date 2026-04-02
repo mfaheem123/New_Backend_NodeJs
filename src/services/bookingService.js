d5e98050e2a74a63b1b6c4995cd818bcc4c0592c
Syed <syedtajuddinirfani@gmail.com>
2026-04-02 16:38:28 +0500

    Driver Busy Socket

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1331,7 +1331,7 @@ async function assignDriverService(bookingId, driverId) {
   const updated = await updateBooking(bookingId, {
     driver_id: driverId,
     booking_status_id: 1,
-    dispatched_at: new Date(),
+    // dispatched_at: new Date(),
   });
 
   if (!updated) return null;
