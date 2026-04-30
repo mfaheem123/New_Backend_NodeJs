0fcfc1d05d0c38258ad3a28c5af3db80047a3ffe
Syed <syedtajuddinirfani@gmail.com>
2026-04-30 22:38:01 +0500

    Driver FOB Check Update

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -14,6 +14,7 @@ router.get("/check-status/:booking_id", bookingController.checkBookingStatus);
 router.get("/driver-rent", bookingController.getBookingByDriverCommission);
 router.get("/customer-bookings/:id", bookingController.getBookingByCustomerId);
 router.get("/customer-jobs", bookingController.getBookingByCustomerMobile);
+router.get("/driver-fob/:id", bookingController.getBookingByDriverIdAndFob);
 
 router.get(
   "/customer-schedule/:id",
